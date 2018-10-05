908711088e20caab752576ba7405bc9688c6b329
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2018-10-05 16:45:24 +0200

    basic code for header/footer/index

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Salut</title>
-    <link rel="stylesheet" href="..css/style.css">
-</head>
-<body>
+<?php get_header(); ?>
+
+
     <h1>Salut !</h1>
-</body>
-</html>
+
+
+<?php get_footer(); ?>
