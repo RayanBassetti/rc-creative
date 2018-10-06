ee474c731148fc4998b4b1034453bd42387cd1d6
Rayan Bassetti <r.bassetti@etu-webschoolfactory.fr>
2018-10-06 11:02:49 +0200

    CSS set up + changed landing page with home.php

---
+++ home.php
@@ -0,0 +1,50 @@
+<?php 
+/* 
+* Template Name: HOME
+*/
+?>
+
+
+<?php get_header(); ?>
+
+<section id="mainBanner">
+    <div id="mainBanner_content">
+        <div id="mainBanner_text">
+            <h1>l'intelligence n'est rien sans ambition <span class="green">.</span></h1>
+            <p>Comme Skip, petit mais puissant.</p>
+        </div>
+        <a href="#" class="button_more">en savoir plus</a>
+    </div>
+</section>
+
+<section id="aboutUs">
+    <div id="aboutUs_blocks">
+        <div id="aboutUs_block">
+            <h1 class="aboutUs_titre">tranquilité d'esprit<span class="green">.</span></h1>
+            <p class="description_text">Nous ferons tout pour vous donner les moyens de réaliser vos projets.</p>
+        </div>
+        <div id="aboutUs_block">
+            <h1 class="aboutUs_titre">tarifications juste<span class="green">.</span></h1>
+            <p class="description_text">Contrairement à la concurrence, notre tarification vous permettra d'avoir un résultat
+                                    à la hauteur de vos attentes.</p>
+        </div>
+        <div id="aboutUs_block">
+            <h1 class="aboutUs_titre">sérieux et professionnalisme<span class="green">.</span></h1>
+            <p class="description_text">Une tarification inférieure n'implique pas une qualité inférieure. Nous nous engageons à produire
+                                    les solutions les plus adaptées pour vous.</p>
+        </div>
+    </div>
+
+</section>
+
+<section id="Portfolio">
+
+</section>
+
+<section id="Equipe">
+    <div id="Equipe_titres">
+        <h1 class="Equipe_titre">notre équipe<span class="green">.</span></h1>
+        <p class="description_text">Nous sommes jeunes mais très expérimentés.</p>
+    </div>
+</section>
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,8 +1,1 @@
-<?php get_header(); ?>
-
-
-    <h1>Salut !</h1>
-    <p>salut</p>
-
-
-<?php get_footer(); ?>
+<p>bonjour.</p>
